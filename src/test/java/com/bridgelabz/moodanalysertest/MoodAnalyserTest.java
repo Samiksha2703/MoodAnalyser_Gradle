7f729785523a464f9a32f9cc7774b443a5a7cc15
Samiksha2703 <shende.samiksha@rediffmail.com>
2021-03-21 00:42:15 +0530

    [Samikhsa].ADD code to analyse test case for message contains sad and no sad

--- src/test/java/com/bridgelabz/moodanalysertest/MoodAnalyserTest.java
+++ src/test/java/com/bridgelabz/moodanalysertest/MoodAnalyserTest.java
@@ -5,13 +5,22 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class MoodAnalyserTest {
+
+    MoodAnalizer moodAnalizer = new MoodAnalizer();
+
     @Test
-    public void testmoodAnalysis() {
+    public void moodAnalysis_WhenMessageCotainSAD_ShouldReturn_SAD() {
 
-        MoodAnalizer moodAnalizer = new MoodAnalizer();
-
-        String mood = moodAnalizer.analyseMood("This is the sad message");
+        String mood = moodAnalizer.analyseMood("I am in Sad Mood");
 
         Assertions.assertEquals("SAD", mood);
+    }
+
+    @Test
+    public void moodAnalysis_WhenMessageDoNotCotainSAD_ShouldReturn_HAPPY() {
+
+        String mood = moodAnalizer.analyseMood("I am in Any Mood");
+
+        Assertions.assertEquals("HAPPY", mood);
     }
 }
